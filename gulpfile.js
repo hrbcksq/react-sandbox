a3af397874bc96adb7a1a72034be5aee1da6762b
hrbcksq <hrbcksq@gmail.com>
2016-10-11 15:33:34 +0300

    Jsx transform

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,44 @@
 var gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
     sourcemaps = require('gulp-sourcemaps'),
-    react = require('gulp-react');
+    babel = require('gulp-babel');
 
 var consts = {
     source: './source',
     public: './public'
 }
 
-gulp.task('serve', ['react'], () => {
+gulp.task('serve', ['babel', 'copy'], () => {
     browserSync.init({
         server: {
-            baseDir: consts.source
+            baseDir: consts.public
         }
     });
 
-    gulp.watch(`${consts.source}/**/*.jsx`, ['react']);
-    gulp.watch(`${consts.source}/**/*.html`).on('change', browserSync.reload)
+    gulp.watch(`${consts.source}/**/*.jsx`, ['babel']);
+    gulp.watch([
+        `${consts.source}/**/*.js`,
+        `${consts.source}/**/*.html`
+    ], ['copy']);
+    // gulp.watch(`${consts.source}/**/*.html`).on('change', browserSync.reload)
 });
 
+gulp.task('copy', () => {
+    return gulp.src([
+            `${consts.source}/**/*.js`,
+            `${consts.source}/**/*.html`
+        ])
+        .pipe(gulp.dest(consts.public))
+        .pipe(browserSync.stream());
+})
 
-gulp.task('react', () => {
+
+gulp.task('babel', () => {
     return gulp.src(`${consts.source}/**/*.jsx`)
         .pipe(sourcemaps.init())
-        .pipe(react())
+        .pipe(babel({
+            plugins: ['transform-react-jsx']
+        }))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(consts.public))
         .pipe(browserSync.stream());
--- source/app.jsx
+++ source/app.jsx
@@ -1,11 +1,25 @@
 var Greeter = React.createClass({
-	render : () => {
+	render : function() {
 		return (
 			<div>				
 				<h1>Hello React!</h1>
 				<p>This is from component!</p>
-			</div>
-		)
+			</div>			
+		);
+		// return React.createElement(
+		// 	'div',
+		// 	null,
+		// 	React.createElement(
+		// 		'h1',
+		// 		null,
+		// 		'Hello React!'
+		// 	),
+		// 	React.createElement(
+		// 		'p',
+		// 		null,
+		// 		'This is from component!'
+		// 	)
+		// );
 	}
 });
 

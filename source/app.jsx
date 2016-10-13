6af00dcc270d3dadc495ca4296e2158eaa38078f
hrbcksq <hrbcksq@gmail.com>
2016-10-13 11:21:05 +0300

    Component set state

--- source/app.jsx
+++ source/app.jsx
@@ -1,3 +1,4 @@
+// state refs props
 var Greeter = React.createClass({
 	getDefaultProps: function() {
 		return {
@@ -5,22 +6,33 @@ var Greeter = React.createClass({
 			message : 'This is from component!'			
 		}
 	},
+	getInitialState: function() {
+		return {
+			name: this.props.name
+		};
+	},
 	onFormSubmit: function(event) {
 		event.preventDefault();
-		var name = this.refs.name.value;
-		alert(name);
+		var input = this.refs.name;
+		var name = input.value;
+		if (typeof name === 'string' && name.length > 0){
+			this.setState({
+				name: name
+			});
+			input.value = '';
+		}
 	},
 
 	render : function() {
-		var name = this.props.name;
+		var name = this.state.name;
 		var message = this.props.message;	
 		return (
 			<div>				
 				<h1>Hello {name}!</h1>
-				<p>{message + '!!'}</p>
+				<p>{message + ' interpolation'}</p>
 				<form onSubmit={this.onFormSubmit}>
 					<input type="text" ref='name'/>
-					<button>Set name</button>
+					<button>Update</button>
 				</form>
 			</div>						
 		);}

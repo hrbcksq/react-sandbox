4a75ea070ae3b5ce90a865b186007898f46b73c0
hrbcksq <hrbcksq@gmail.com>
2016-10-13 12:30:35 +0300

    Nested components (presentation + response) in container component

--- source/app.jsx
+++ source/app.jsx
@@ -1,47 +1,67 @@
-// state refs props
-var Greeter = React.createClass({
+//Presentation component
+var GreeterHeader = React.createClass({
 	getDefaultProps: function() {
 		return {
-			name : 'Default name',
-			message : 'This is from component!'			
-		}
-	},
-	getInitialState: function() {
-		return {
-			name: this.props.name
+			name : 'Default Name',
+			message : 'Default Message'
 		};
 	},
-	onFormSubmit: function(event) {
-		event.preventDefault();
-		var input = this.refs.name;
-		var name = input.value;
-		if (typeof name === 'string' && name.length > 0){
-			this.setState({
-				name: name
-			});
-			input.value = '';
-		}
-	},
-
-	render : function() {
-		var name = this.state.name;
-		var message = this.props.message;	
+	render: function() {
+		var name = this.props.name;		
+		var message = this.props.message;
 		return (
-			<div>				
-				<h1>Hello {name}!</h1>
-				<p>{message + ' interpolation'}</p>
-				<form onSubmit={this.onFormSubmit}>
-					<input type="text" ref='name'/>
-					<button>Update</button>
-				</form>
-			</div>						
-		);}
+			<div>
+				<h1>Hello {name}</h1>
+				<p>{message}</p>
+			</div>
+		);
+	}
 });
 
-var firstName = 'John';
-var message = 'Hello React';
+//Response component
+var GreeterForm = React.createClass({
+	changeName: function(event) {
+		event.preventDefault();
+		var name = this.refs.name.value;
+		if (typeof name === 'string' && name.length > 0){
+			this.refs.name.value = '';
+			this.props.changeName(name);
+		}
+	},
+	render: function() {
+		return (
+			<form onSubmit={this.changeName}>
+				<input type="text" ref='name'/>
+				<button>Update Name</button>
+			</form>
+		);
+	}
+});
+
+//Container component (it maintains state and render children)
+var Greeter = React.createClass({
+	getInitialState: function() {
+		return {
+			name: this.props.name,
+			message: this.props.message 
+		};
+	},
+	updateName: function(name) {
+		this.setState({
+			name: name
+		});
+	},
+	render: function() {
+		return (
+			<div>
+				<GreeterHeader name={this.state.name} message={this.state.message}/>
+				<GreeterForm changeName={this.updateName}/>
+			</div>			
+		);
+	}
+});
 
 ReactDOM.render(
-	<Greeter name={firstName} message={message}/>,	
+	<Greeter/>,
 	document.getElementById('app')
 )
